d6ffdd643ef428125c0c5b5ac09d027f3c0240ef
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2012-05-24 15:37:57 -0400

    Doh! script directive should go before the comment block!

--- convert.py
+++ convert.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #########################################################################
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #########################################################################
-
-#!/usr/bin/env python
 
 """
     Conversion Utility for C#.NET to Python.
